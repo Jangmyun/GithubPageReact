1a83bbbef5e72ef0c3725972ffb947e1027eab0e
Jangmyun <cndlsrb2739@gmail.com>
2023-09-09 16:51:09 +0900

    Routes Route import

--- github_page_react/src/App.js
+++ github_page_react/src/App.js
@@ -1,9 +1,12 @@
 import './css/App.css';
+import { useState } from 'react';
+import {Routes, Route, Link} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      
+
     </div>
   );
 }
