4ab65fef80ad836b811983cc61cc7b8a693157d5
Jangmyun <cndlsrb2739@gmail.com>
2023-09-17 01:44:56 +0900

    라우터로 페이지 나누기 (order & delivery )

--- github_page_react/src/App.js
+++ github_page_react/src/App.js
@@ -1,12 +1,27 @@
 import './css/App.css';
+import Nav from './components/Nav';
 import { useState } from 'react';
 import {Routes, Route, Link} from 'react-router-dom';
+import {OrderList, OrderForm} from './components/Order';
 
 
 function App() {
+  let [isOptionSelected, setIsOptionSelected] = useState(false);
   return (
     <div className="App">
-
+      <Nav isSelected={isOptionSelected} setIsSelected={setIsOptionSelected}></Nav>
+      {
+        isOptionSelected ? 
+        <>
+        <div>보여줄거</div> 
+        <Routes>
+          <Route path='/order' element={ <OrderList></OrderList>}></Route> {/* /order 페이지 들어가면  orderList 보여주기*/}
+          <Route path='/delivery'></Route>
+        </Routes>
+        </>
+        : null
+      }
+      
     </div>
   );
 }
--- github_page_react/src/components/Order.js
+++ github_page_react/src/components/Order.js
@@ -1,9 +1,25 @@
+import {useEffect} from 'react';
+
 function OrderList(){
+    
     return(
         <div className="order-list">
-            
+            <p>보여줄거</p>
         </div>
     );
 }
+function OrderForm(){
+    useEffect(()=>{
+        const mapScriptTag = document.createElement('script');
+        mapScriptTag.src = "//dapi.kakao.com/v2/maps/sdk.js?appkey=54ee9989a4bb5b14eab732e07bd561cc"
+        mapScriptTag.async = true;
+        document.appendChild(mapScriptTag);
+    });
+    return(
+        <>
+        <div id='map'></div>
+        </>
+    );
+}
 
-export {OrderList};
+export {OrderList, OrderForm};
