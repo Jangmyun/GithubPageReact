60d61700769273f3d22bb8f7fea3800a7f1f964a
Jangmyun <cndlsrb2739@gmail.com>
2023-09-19 00:54:06 +0900

    옵션 버튼 onclick 이벤트 수정 (href 수정 및 css 스타일)

--- github_page_react/src/components/Nav.js
+++ github_page_react/src/components/Nav.js
@@ -1,28 +1,22 @@
 import '../css/Nav.css';
-import {useState} from 'react';
 
-function Nav(props){    //NavigationBar HTML 마크업
-    let [titleMarginTop, setTitleMarginTop] = useState( {margin:'60% 0 0.67em 0'});
-    
+function Nav(props){    //NavigationBar HTML 마크업    
     return(
         <div className="navbar">
-            <div className="navbar-title" style={titleMarginTop}>
+            <div className="navbar-title" onClick={()=>{
+                window.location.href = "/";
+            }} >
                 <h1>배달비좀 아끼자</h1>
             </div>
 
             <div className='navbar-option'>
                 <div className='option-customer, option' onClick={()=>{
-                    setTitleMarginTop({margin: '30px 0 -20px 0'});
-                    // setTimeout 함수로 option 버튼 터치시 
-                    setTimeout(()=>{props.setIsSelected(true);} , 800);
                     window.location.href = "/order";
                 }}>
                     <span>주문할거임? 🤔</span>
                 </div>
                 <div className='option-rider, option' onClick={()=>{
-                    setTitleMarginTop({margin: '30px 0 -20px 0'});
-                    setTimeout(()=>{props.setIsSelected(true);} , 1000);
-                    window.location.href = "/delivery";
+                    window.location.href = "/delivery";                    
                 }}>
                     <span>배달할거임? 🚙</span>
                 </div>
